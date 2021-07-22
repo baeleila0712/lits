b8f8296bf71d31fadb5a12dda60c5c186a3716c9
BAe <bae0712bar@gmail.com>
2021-07-22 13:51:19 +0800

    training

---
+++ train.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+train_loader = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+validation_loader = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+
+model = get_unet()
+
+history = model.fit(x_train, y_train, batch_size=10, epochs=20, verbose=1, shuffle=True,
+              validation_data=validation_loader)
