bd45d6baba3b04e40a7a489a03c031aea7d3b12b
BAe <bae0712bar@gmail.com>
2021-07-26 13:17:14 +0800

    loader

--- train.py
+++ train.py
@@ -5,5 +5,9 @@ validation_loader = tf.data.Dataset.from_tensor_slices((x_val, y_val))
 
 model = get_unet()
 
-history = model.fit(x_train, y_train, batch_size=10, epochs=20, verbose=1, shuffle=True,
-              validation_data=validation_loader)
+history = model.fit(train_loader, 
+	batch_size=10, 
+	epochs=20, 
+	verbose=1, 
+	shuffle=True,
+	validation_data=validation_loader)
